232a767dbdfb73870fd1496cb8113f40288d2d45
grandonk666 <57865208+grandonk666@users.noreply.github.com>
2019-12-06 02:28:02 +0700

    Update and rename 2.sh to Shell Programing 2.sh

---
+++ Legacy Codes/Shell Programing 2.sh
@@ -0,0 +1,42 @@
+exit=1;
+while [[$exit -eq 1 ]]
+do
+echo “1. Menu Kalkulator Loop”;
+echo “2. Keluar”;
+echo “Pilihan : ”;
+read pilihan
+if [[ $pilihan -eq 1 ]];
+then
+ echo “Masukan Bilangan Acuan : ”;
+ read acuan;
+ echo “Masukan Batasan Loop : ”;
+ read loop;
+ echo “Penjumlahan”;
+ for ((i=1; i<=loop; i=i+1))
+ do
+  let jumlah=$acuan+$i
+  echo “$acuan + $i = $jumlah”;
+ done
+ echo “Pengurangan”;
+ for ((i=1; i<=loop; i=i+1))
+ do
+  let jumlah=$acuan-$i
+  echo “$acuan - $i = $jumlah”;
+ done
+ echo “Pembagian”;
+ for ((i=1; i<=loop; i=i+1))
+ do
+  let jumlah=$acuan/$i
+  echo “$acuan / $i = $jumlah”;
+ done
+ echo “Perkalian”;
+ for ((i=1; i<=loop; i=i+1))
+ do
+  let jumlah=$acuan*$i
+  echo “$acuan * $i = $jumlah”;
+ done
+elif [[ $pilihan = 2 ]]
+then
+ exit=0;
+ fi
+done
